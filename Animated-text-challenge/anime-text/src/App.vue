ee3ace7414606e7a17752c5cf369f2fe23a1a80e
iamdev-james <jamesoluwabukola87@gmail.com>
2022-02-20 15:15:22 -0100

    Added task 1 and 2

---
+++ Animated-text-challenge/anime-text/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id='box' class='box'></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.box {
+  perspective: 350px;
+  margin: auto;
+	width:280px;
+	height:280px;
+	display:flex;
+	align-items:center;
+  overflow: hidden;
+	justify-content:center;
+	font-family: "Inconsolata", monospace;
+	font-size: 28px;
+	border:1px solid #c39f76;
+	box-sizing:border-box;
+}
+#box:before {
+  content: 'Hello World Hello World Hello World Hello World Hello World Hello World Hello World';
+  text-align: center;
+  width: 200px;
+  transform: rotateX(60deg) translateY(170%);
+  animation: scroll 10s linear infinite;
+  line-height: 50px;
+}
+@keyframes scroll {
+ from {
+  transform: rotateX(65deg) translateY(160%);
+ }
+ to {
+  transform: rotateX(65deg) translateY(-50%);
+ }
+}
+</style>
---
+++ QR-code-component-challenge/myqr-code/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<main class= "h-screen bg-blue-100 flex justify-center items-center">
+ <div class= "w-90 flex flex-col justify-center items-center bg-white py-5 px-6 rounded-xl shadow-gray-200">
+  <div class= "w-72 bg-blue-300 flex justify-center p-10 rounded-xl">
+  <img alt="my-qr-code" class= "w-40" src="./assets/myqr-code.png">
+  </div>
+  <div class= "w-72 text-center pt-6">
+    <h2 class= "pb-4 text-2xl font-semibold">Improve your front-end skills by building projects</h2>
+    <p class= "text-slate-600 opacity-80">Scan the QR code to view my profile, let's work on your next project together</p>
+  </div>
+ </div>
+</main> 
+</template>
+
+<script>
+import './assets/tailwind.css'
+
+export default {
+  
+}
+</script>
